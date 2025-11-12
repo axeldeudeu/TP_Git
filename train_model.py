1c4cfc5149a68fea5460b39b17a6f743a57aa9c4
“axeldeudeu“ <axel.derouet2003@gmail.com>
2025-11-12 11:43:59 +0100

    Amélioration de train_model.py avec validation

--- train_model.py
+++ train_model.py
@@ -1,8 +1,14 @@
-def train_model(train_X, train_y, test_X, model):
-    """entraine un modele ML de classification et retourne des predictions sur
-    le dataset de test"""
-    model.fit(train_X,train_y) 
-    prediction=model.predict(test_X)
-    return prediction
-
-
+def train_model(train_X, train_y, model):
+    """
+    Entraîne un modèle ML de classification et retourne le modèle entraîné
+    
+    Modification par Etudiant A:
+    - Ajout de validation des paramètres
+    - Amélioration de la documentation
+    """
+    # Validation des données
+    if train_X is None or train_y is None:
+        raise ValueError("Les données d'entraînement ne peuvent pas être None")
+    
+    model.fit(train_X, train_y)
+    return model
